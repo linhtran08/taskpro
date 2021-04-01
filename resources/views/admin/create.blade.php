042a060795272f9116b55e81edc10c6d68690f89
Trung Nguyen <trung.ng.ueh@gmail.com>
2021-04-01 16:49:01 +0700

    tiny fix

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -150,10 +150,10 @@
                                             </div>
                                         </li>
                                     </ul>
-                                    <div class="custom-control custom-checkbox mt-4">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">I have read and agreed to the terms of services and privacy policy</label>
-                                    </div>
+{{--                                    <div class="custom-control custom-checkbox mt-4">--}}
+{{--                                        <input type="checkbox" class="custom-control-input" id="customCheck1">--}}
+{{--                                        <label class="custom-control-label" for="customCheck1">I have read and agreed to the terms of services and privacy policy</label>--}}
+{{--                                    </div>--}}
                                 </div>
                             </section>
                         </form>
