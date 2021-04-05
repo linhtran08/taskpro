5456aa8726d51924449d37c5ef7e1fad103f435a
Trung Nguyen <trung.ng.ueh@gmail.com>
2021-04-05 16:29:13 +0700

    update Monitor controller(view), other fixes

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,7 +11,8 @@ class ProjectController extends Controller
         $this->validate($request,[
             'project_title' => 'required',
             'prj_start_date' => 'required',
-            'prj_due_date' => 'required'
+            'prj_due_date' => 'required',
+            'project_detail' => 'required',
         ]);
 
         $created_by_id = session()->get('account.emp_id');
@@ -19,14 +20,18 @@ class ProjectController extends Controller
         $start_date = $request->input('prj_start_date');
         $end_date = $request->input('prj_due_date');
         $project_name = $request->input('project_title');
+        $project_detail = $request->input('project_detail');
+        $project_state_id = 1; //default state for project is open
 
         DB::table('project')
             ->insert([
                 'project_name' => $project_name,
                 'project_pic_id' => $created_by_id,
+                'project_state_id' => $project_state_id,
                 'created_at' => $created_at,
                 'prj_start_date' => $start_date,
-                'prj_due_date' => $end_date
+                'prj_due_date' => $end_date,
+                'prj_detail' => $project_detail,
             ]);
         return back();
     }
