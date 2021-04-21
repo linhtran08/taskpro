4be921831c115b82f58b8ce21618532346833fd4
Trung Nguyen <trung.ng.ueh@gmail.com>
2021-04-21 15:20:16 +0700

    turn setting button, notification button and some image at login

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,28 +9,28 @@
         </div>
     </div>
     <div class="header-right">
-        <div class="dashboard-setting user-notification">
-            <div class="dropdown">
-                <a class="dropdown-toggle no-arrow" href="javascript:;" data-toggle="right-sidebar">
-                    <i class="dw dw-settings2"></i>
-                </a>
-            </div>
-        </div>
-        <div class="user-notification">
-            <div class="dropdown buzz">
-                <a class="dropdown-toggle no-arrow" href="{{ url('profile') }}" role="button" data-toggle="dropdown">
-                    <i class="icon-copy dw dw-notification"></i>
-                    <span class="badge notification-active"></span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <div class="notification-list mx-h-350 customscroll">
-                        <ul id="message_render">
+{{--        <div class="dashboard-setting user-notification">--}}
+{{--            <div class="dropdown">--}}
+{{--                <a class="dropdown-toggle no-arrow" href="javascript:;" data-toggle="right-sidebar">--}}
+{{--                    <i class="dw dw-settings2"></i>--}}
+{{--                </a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="user-notification">--}}
+{{--            <div class="dropdown buzz">--}}
+{{--                <a class="dropdown-toggle no-arrow" href="{{ url('profile') }}" role="button" data-toggle="dropdown">--}}
+{{--                    <i class="icon-copy dw dw-notification"></i>--}}
+{{--                    <span class="badge notification-active"></span>--}}
+{{--                </a>--}}
+{{--                <div class="dropdown-menu dropdown-menu-right">--}}
+{{--                    <div class="notification-list mx-h-350 customscroll">--}}
+{{--                        <ul id="message_render">--}}
 
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--                        </ul>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
         <div class="user-info-dropdown">
             <div class="dropdown">
                 <a class="dropdown-toggle" href="{{ url('profile') }}" role="button" data-toggle="dropdown">
--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -272,70 +272,70 @@
 
         <!-- Uncomment below if you wan to use dynamic maps -->
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1385.6873264808733!2d106.6664137272078!3d10.787120946489539!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd2ecb62e0d050fe9!2sFPT-Aptech%20Computer%20Education%20HCM!5e0!3m2!1svi!2s!4v1616612789196!5m2!1svi!2s" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
-        <div class="container mt-5">
-            <div class="row justify-content-center">
+{{--        <div class="container mt-5">--}}
+{{--            <div class="row justify-content-center">--}}
 
-                <div class="col-lg-3 col-md-4">
+{{--                <div class="col-lg-3 col-md-4">--}}
 
-                    <div class="info">
-                        <div>
-                            <i class="fa fa-map-marker"></i>
-                            <p>CMT8 Street<br>HO CHI MINH</p>
-                        </div>
-                        <div>
-                            <i class="fa fa-envelope"></i>
-                            <p>taskpro@pro.vn</p>
-                        </div>
+{{--                    <div class="info">--}}
+{{--                        <div>--}}
+{{--                            <i class="fa fa-map-marker"></i>--}}
+{{--                            <p>CMT8 Street<br>HO CHI MINH</p>--}}
+{{--                        </div>--}}
+{{--                        <div>--}}
+{{--                            <i class="fa fa-envelope"></i>--}}
+{{--                            <p>taskpro@pro.vn</p>--}}
+{{--                        </div>--}}
 
-                        <div>
-                            <i class="fa fa-phone"></i>
-                            <p>0909999999</p>
-                        </div>
-                    </div>
+{{--                        <div>--}}
+{{--                            <i class="fa fa-phone"></i>--}}
+{{--                            <p>0909999999</p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
 
-                    <div class="social-links">
-                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-                        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-                        <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-                        <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-                    </div>
+{{--                    <div class="social-links">--}}
+{{--                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>--}}
+{{--                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>--}}
+{{--                        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>--}}
+{{--                        <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>--}}
+{{--                        <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>--}}
+{{--                    </div>--}}
 
-                </div>
+{{--                </div>--}}
 
-                <div class="col-lg-5 col-md-8">
-                    <div class="form">
+{{--                <div class="col-lg-5 col-md-8">--}}
+{{--                    <div class="form">--}}
 {{--                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">--}}
-                        <form action="{{ route('login') }}" role="form" class="php-email-form">
-                            <div class="form-group">
-                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                <div class="validate"></div>
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                                <div class="validate"></div>
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                                <div class="validate"></div>
-                            </div>
-                            <div class="form-group">
-                                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                                <div class="validate"></div>
-                            </div>
-                            <div class="mb-3">
-                                <div class="loading">Loading</div>
-                                <div class="error-message"></div>
-                                <div class="sent-message">Your message has been sent. Thank you!</div>
-                            </div>
-                            <div class="text-center"><button type="submit">Send Message</button></div>
-                        </form>
-                    </div>
-                </div>
+{{--                        <form action="{{ route('login') }}" role="form" class="php-email-form">--}}
+{{--                            <div class="form-group">--}}
+{{--                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />--}}
+{{--                                <div class="validate"></div>--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />--}}
+{{--                                <div class="validate"></div>--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />--}}
+{{--                                <div class="validate"></div>--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>--}}
+{{--                                <div class="validate"></div>--}}
+{{--                            </div>--}}
+{{--                            <div class="mb-3">--}}
+{{--                                <div class="loading">Loading</div>--}}
+{{--                                <div class="error-message"></div>--}}
+{{--                                <div class="sent-message">Your message has been sent. Thank you!</div>--}}
+{{--                            </div>--}}
+{{--                            <div class="text-center"><button type="submit">Send Message</button></div>--}}
+{{--                        </form>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
-            </div>
+{{--            </div>--}}
 
-        </div>
+{{--        </div>--}}
     </section><!-- End Contact Section -->
 
     <!-- ======= Footer ======= -->
